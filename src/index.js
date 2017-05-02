5056090cc66738d7cd672c83696ba303bb52edc0
Sittipong HAUS <me@sittipong.com>
2017-05-02 16:14:06 +0700

    initial package

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React, { Component, PropTypes } from 'react'
-import { UniqueId } from 'components/Utility'
+import { UniqueId } from 'sendit-utility'
+import './style.css'
 export default class SenditInput extends Component {
-  static propTypes = {
-    label: PropTypes.string,
-    value: PropTypes.string,
-    type: PropTypes.string || 'text',
-    placeholder: PropTypes.string,
-    minLength: PropTypes.number || 0,
-    maxLength: PropTypes.number || 255,
-    rows: PropTypes.number || 3,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    onKeyPress: PropTypes.func,
-    onChange: PropTypes.func,
-    disabled: PropTypes.bool,
-    onEnter: PropTypes.func
-  }
-
   constructor() {
     super()
     this.state = {
@@ -26,12 +11,10 @@ export default class SenditInput extends Component {
     this.onChange = this.onChange.bind(this)
     this.onKeyPress = this.onKeyPress.bind(this)
   }
-
   componentDidMount () {
     this.inputClassFunc()
     this.setState({uniqueId: UniqueId(6)})
   }
-
   onChange (e) {
     const { onChange } = this.props
     onChange(e)
@@ -39,17 +22,14 @@ export default class SenditInput extends Component {
       this.inputClassFunc()
     }, 0)
   }
-
   onKeyPress (e) {
     const { onKeyPress, onEnter } = this.props
     onKeyPress && onKeyPress(e)
-    // console.log(e)
     if ( e.key === 'Enter' ) {
       console.log('onEnter')
       onEnter && onEnter(e)
     }
   }
-
   inputClassFunc () {
     const { value, type, placeholder, minLength, maxLength } = this.props
     const { inputClass } = this.state
@@ -61,11 +41,10 @@ export default class SenditInput extends Component {
 
     this.setState({inputClass: inputClassList})
   }
-
   render() {
     const { label, value, type, placeholder, minLength, maxLength, rows, onFocus, onBlur, onChange, onKeyPress, disabled } = this.props
     const { values, inputClass, uniqueId } = this.state
-    console.log('inputClass', inputClass)
+    // console.log('inputClass', inputClass)
     return (
       <div className={'SenditInput' + inputClass}>
         {(type==='textarea')?<textarea id={uniqueId} className={'textarea'} value={value || ''} minLength={minLength} maxLength={maxLength} onFocus={onFocus} onBlur={onBlur} onChange={this.onChange} onKeyPress={this.onKeyPress} rows={rows} disabled={disabled}></textarea>:<input id={uniqueId} className={'input'} type={type || 'text'} minLength={minLength} maxLength={maxLength} value={value || ''} onFocus={onFocus} onBlur={onBlur} onChange={this.onChange} onKeyPress={this.onKeyPress} disabled={disabled} />}
@@ -74,4 +53,25 @@ export default class SenditInput extends Component {
       </div>
     )
   }
+}
+SenditInput.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  minLength: PropTypes.number,
+  maxLength: PropTypes.number,
+  rows: PropTypes.number,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+  onEnter: PropTypes.func
+}
+SenditInput.defaultProps = {
+  type: 'text',
+  minLength: 0,
+  maxLength: 255,
+  rows: 3
 }
